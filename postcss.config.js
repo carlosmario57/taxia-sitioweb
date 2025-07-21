35060de40b7588ae3ff446254ed4ff00668e3905
carlosmario57 <camafug@gmail.com>
2025-07-21 17:39:11 -0500

    Actualiza configuración de PostCSS para Tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   plugins: {
-    tailwindcss: {},
+    // Cambiamos 'tailwindcss' a require('@tailwindcss/postcss')
+    // Esto le dice a PostCSS que use el plugin de Tailwind CSS de la nueva manera.
+    '@tailwindcss/postcss': {}, 
     autoprefixer: {},
   },
 }

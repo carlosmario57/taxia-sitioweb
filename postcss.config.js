755d65ad8925b8d9312fb53a0a8eadeca787d82c
carlosmario57 <camafug@gmail.com>
2025-07-21 20:05:57 -0500

    Confirma configuración correcta de PostCSS para Tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,5 @@
 module.exports = {
   plugins: {
-    // Cambiamos 'tailwindcss' a require('@tailwindcss/postcss')
-    // Esto le dice a PostCSS que use el plugin de Tailwind CSS de la nueva manera.
     '@tailwindcss/postcss': {}, 
     autoprefixer: {},
   },
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // Busca clases de Tailwind en todos los archivos JS, JSX, TS, TSX dentro de la carpeta src
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
